293c4438d0d93ae5c3cff78d2f99d06ab26cc351
Muhamad Irwan <muhamadirwan99@gmail.com>
2026-02-26 06:36:19 +0700

    implement custom broadcast receiver for download status in MainActivity
    - Define `ACTION_DOWNLOAD_STATUS` and implement a `BroadcastReceiver` to handle download completion notifications.
    - Register `downloadReceiver` using `ContextCompat.registerReceiver` with `RECEIVER_NOT_EXPORTED` for security.
    - Implement a simulated download trigger using `Handler` and `sendBroadcast` with a 3-second delay.
    - Ensure proper resource management by unregistering the receiver in `onDestroy()`.
    - Update `onClick` logic and UI layout to include a "Download File" button.
    - Update `gradle.xml` with migration settings.

--- app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.dicoding.mybroadcastreceiver
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+    }
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var downloadReceiver: BroadcastReceiver
+
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,22 +49,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         binding.btnPermission.setOnClickListener(this)
-    }
+        binding.btnDownload.setOnClickListener(this)
 
-    var requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
         }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        ContextCompat.registerReceiver(
+            this,
+            downloadReceiver,
+            downloadIntentFilter,
+            ContextCompat.RECEIVER_NOT_EXPORTED,
+        )
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+            R.id.btn_download -> {
+                Handler(Looper.getMainLooper()).postDelayed(
+                    {
+                        val notifyFinishIntent = Intent(ACTION_DOWNLOAD_STATUS)
+                        notifyFinishIntent.setPackage(packageName)
+                        sendBroadcast(notifyFinishIntent)
+                    },
+                    3000
+                )
+            }
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(downloadReceiver)
     }
 }
