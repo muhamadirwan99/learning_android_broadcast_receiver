ea7b0c2cf64f352bfbe70c8c053709d6af6ea8ef
Muhamad Irwan <muhamadirwan99@gmail.com>
2026-02-25 21:58:24 +0700

    implement SMS receiving functionality with BroadcastReceiver and permission handling
    - Add `SmsReceiver` to intercept incoming SMS and extract message data.
    - Create `SmsReceiverActivity` to display incoming message details as a dialog.
    - Update `MainActivity` to request `RECEIVE_SMS` permission using `ActivityResultContracts`.
    - Enable `viewBinding` and update layouts for permission checking and message display.
    - Configure `AndroidManifest.xml` with required permissions and receiver/activity declarations.

--- app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.dicoding.mybroadcastreceiver
 
+import android.Manifest
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dicoding.mybroadcastreceiver.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnPermission.setOnClickListener(this)
+    }
+
+    var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+        }
+
     }
 }
